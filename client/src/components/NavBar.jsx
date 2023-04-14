46ca6aa09ff189cf7fc55a108ea86164d2227dfb
Ben Baldwin <baldwin.ben55@gmail.com>
2023-04-14 14:23:29 -0700

    adding responsive sizing and trying to add smooth transitions

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,12 +4,12 @@ import {faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const NavBar = () => {
   return (
-    <div className='h-24 flex flex-col items-center md:flex-row justify-between bg-gradient-to-b from-zinc-950 via-zinc-900 to-zinc-800' id='navBackground'>
-      <p className='hidden sm:block text-3xl w-32 text-center text-cyan-100 font-extrabold'>BB</p>
+    <div className='pt-4 lg:pt-0 h-24 flex flex-col items-center md:flex-row justify-between bg-gradient-to-b from-zinc-950 via-zinc-900 to-zinc-800' id='navBackground'>
+      <p className='hidden md:block text-3xl w-32 text-center text-cyan-100 font-extrabold'>BB</p>
       <div className=' text-cyan-100 font-extrabold uppercase flex gap-12'>
-        <a className='text-2xl hover:text-3xl transition-all duration-200 transform hover:scale-110' href="#">Work</a>
+        <a className='text-2xl hover:text-3xl transition-all duration-200 transform hover:scale-110' href='#work'>Work</a>
         <a className='text-2xl hover:text-3xl transition-all duration-200 transform hover:scale-110' href="#">Resume</a>
-        <a className='text-2xl hover:text-3xl transition-all duration-200 transform hover:scale-110' href="#">contact</a>
+        <a className='text-2xl hover:text-3xl transition-all duration-200 transform hover:scale-110' href="#contact">contact</a>
       </div>
       <div className='w-32 space-x-4 text-center'>
         <a className='text-blue-600 hover:text-xl transition-all duration-200' target='_blank' href="https://www.linkedin.com/in/ben-baldwin55/">
--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/NavBar'
 import Skill from '../components/Skill'
 import Project from '../components/Project'
 import viewOne16x9 from '../assets/viewOne16x9.PNG'
+import Contact from '../components/Contact'
 
 const Homepage = () => {
 
@@ -10,67 +11,70 @@ const Homepage = () => {
   const frontEnd = ['React', 'JSX', 'Jinja2', 'Bootstrap', 'Tailwind']
   const backEnd = ['Flask', 'Spring Boot', 'Django', 'Express']
   const databases = ['Mongo', 'MySQL', 'Mongoose']
+  const apis = ['Stripe', 'EmailJS']
   const aws = ['AWS', 'EC2', 'S3 Bucket', 'Cloud Front', 'Route 53', 'SSL', 'Deployment']
   const tools = ['Git', 'GitHub']
   const gis = ['JOSM', 'Esri', 'QGIS', 'AutoCAD']
 
   return (
-    <main className='bg-cover bg-center min-h-screen'>
-      <NavBar />
-      <body>
-        <section className='max-w-screen-xl mx-auto mt-8'>
-          {/* Hero */}
-          <div className='flex flex-col sm:flex-row justify-between gap-12'>
-            <img className=' h-5/12 w-5/12 object-cover shadow-lg rounded-xl' src="/coloredSquare.png" alt="Head shot" />
-            <div className='flex flex-col justify-end bg-zinc-700 shadow-lg rounded-xl self-end p-4'>
-              <h1 className='text-6xl font-bold mb-4'>Hi, I'm Ben and I'm a full stack Web Developer.</h1>
-              <p className='text-xl p-2'>Welcome to my portfolio! I'm Ben, a passionate full stack web developer based in Seattle, Washington. 
-              With a strong foundation in full stack web development, I'm always looking for new opportunities to grow my skillset. 
-              I'm a recent graduate from Coding Dojo, where I gained hands-on experience in building dynamic web applications using languages such as JavaScript, Python, Java, and more. 
-              I believe that great software should be both functional and aesthetically pleasing, and I strive to create user-friendly interfaces with clean code. 
-              I am actively seeking employment opportunities.</p>
-              <div className='space-x-4 mt-4 flex'>
-                <button className='rounded text-lg flex-grow border-2 px-6 py-2 font-medium hover:bg-cyan-800 active:bg-gray-800'>Contact Me</button>
-                <button className='rounded text-lg flex-grow border-2 px-6 py-2 font-medium hover:bg-cyan-800 active:bg-gray-800'>View my work</button>
+      <main className='bg-cover bg-center min-h-screen'>
+        <NavBar />
+        <body>
+          <section className='max-w-screen-xl mx-auto mt-8'>
+            {/* Hero */}
+            <div className='flex flex-col sm:flex-row justify-between gap-12'>
+              <img className=' h-5/12 w-5/12 object-cover shadow-lg rounded-xl' src="/coloredSquare.png" alt="Head shot" />
+              <div className='flex flex-col justify-end bg-zinc-700 shadow-lg rounded-xl self-end p-4'>
+                <h1 className='text-6xl font-bold mb-4'>Hi, I'm Ben and I'm a full stack Software Developer.</h1>
+                <p className='text-xl p-2'>Welcome to my portfolio! I'm Ben, a passionate full stack web developer based in Seattle, Washington.
+                  With a strong foundation in full stack web development, I'm always looking for new opportunities to grow my skillset.
+                  I'm a recent graduate from Coding Dojo, where I gained hands-on experience in building dynamic web applications using languages such as JavaScript, Python, Java, and more.
+                  I believe that great software should be both functional and aesthetically pleasing, and I strive to create user-friendly interfaces with clean code.
+                  I am actively seeking employment opportunities.</p>
+                <div className='space-x-4 mt-4 flex'>
+                  <button className='rounded text-lg flex-grow border-2 px-6 py-2 font-medium hover:bg-cyan-800 active:bg-gray-800'>Contact Me</button>
+                  <button className='rounded text-lg flex-grow border-2 px-6 py-2 font-medium hover:bg-cyan-800 active:bg-gray-800'>View my work</button>
+                </div>
               </div>
             </div>
-          </div>
-          {/* Work */}
-          <div className='mt-16 bg-zinc-700 shadow-lg p-6 rounded-xl'>
-            <p className='text-5xl font-bold mb-4'>Skills and Projects</p>
-            <p className='text-xl mb-2'>Below is a list of skills I have acquired and am continuously improving upon.</p>
-            {/* Skills */}
-            <div>
-              {
-                languages.concat(frontEnd, backEnd, databases, aws, tools, gis).map((skill, i) => {
-                  return <Skill skill={skill} />
-                })
-              }
+            {/* Work */}
+            <div className='mt-16 bg-zinc-700 shadow-lg p-6 rounded-xl'>
+              <p className='text-5xl font-bold mb-4'>Skills and Projects</p>
+              <p className='text-xl mb-2'>Below is a list of skills I have acquired and am continuously improving upon.</p>
+              {/* Skills */}
+              <div>
+                {
+                  languages.concat(frontEnd, backEnd, databases, apis, aws, tools, gis).map((skill, i) => {
+                    return <Skill skill={skill} />
+                  })
+                }
+              </div>
             </div>
-          </div>
-        </section>
-        {/* Projects */}
-        <section className='mt-16 max-w-screen-xl mx-auto space-y-14'>
-          <Project
-            name={'Ecommerce App'}
-            projectDescription={'An AWS deployed fullstack MERN Drumming Ecommerce App featuring admin login/reg, multer, and Stripe.'}
-            appLink={'https://www.percussionpro.store'}
-            appImage={viewOne16x9}
-            appRepo={'https://github.com/ben-baldwin/DrumEcommerce'}
-            skills={'JavaScript | React | AWS | Mongo | GitHub | Node | Stripe | Multer | JSX | Tailwind | Express'}
-          />
-          <Project
-            name={'Airline Reservation App'}
-            projectDescription={'An AWS deployed fullstack Java Airline Reservation System App featuring admin and user login/reg and Stripe.'}
-            appLink={'#'}
-            appImage={viewOne16x9}
-            appRepo={'https://github.com/jordanruhle/Airline-Reservation-System'}
-            skills={'Java | Bootstrap | Spring Boot | Java | AWS | GitHub | Stripe'}
-          />
-
-        </section>
-      </body>
-    </main>
+          </section>
+          {/* Projects */}
+          <section className='mt-16 max-w-screen-xl mx-auto space-y-14' id="work">
+            <Project
+              name={'Ecommerce App'}
+              projectDescription={'An AWS deployed fullstack MERN Drumming Ecommerce App featuring admin login/reg, multer, and Stripe.'}
+              appLink={'https://www.percussionpro.store'}
+              appImage={viewOne16x9}
+              appRepo={'https://github.com/ben-baldwin/DrumEcommerce'}
+              skills={'JavaScript | React | AWS | Mongo | GitHub | Node | Stripe | Multer | JSX | Tailwind | Express'}
+            />
+            <Project
+              name={'Airline Reservation App'}
+              projectDescription={'An AWS deployed fullstack Java Airline Reservation System App featuring admin and user login/reg and Stripe.'}
+              appLink={'#'}
+              appImage={viewOne16x9}
+              appRepo={'https://github.com/jordanruhle/Airline-Reservation-System'}
+              skills={'Java | Bootstrap | Spring Boot | Java | AWS | GitHub | Stripe'}
+            />
+          </section>
+          <section className='max-w-screen-xl mx-auto'>
+            <Contact />
+          </section>
+        </body>
+      </main>
   )
 }
 
